8931326641ca010c7ddc4f22657f69c7aa3a5e93
Gregoire Parant <g.parant@thecodingmachine.com>
2021-09-30 17:04:21 +0200

    Update script

--- Seroni/le-onze/script.js
+++ Seroni/le-onze/script.js
@@ -1,3 +1,6 @@
+/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+import {bootstrapExtra} from '@workadventure/scripting-api-extra'
+
 WA.onInit().then(() => {
     console.log('Current player name: ', WA.player.name);
 });
